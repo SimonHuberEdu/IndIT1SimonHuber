43a180c0dc0e94c66bf4a9ea61010bf49e55f8c4
SimonHuberEdu <80050945+SimonHuberEdu@users.noreply.github.com>
2021-03-26 13:17:06 +0100

    Add files via upload

---
+++ .github/workflows/Aufgabe 1 Py Simon Huber.py
@@ -0,0 +1,22 @@
+#aufgabe 1
+#Benutzer begrüßen
+#zahl entgegennehmen
+#zweite zahl entgegennehmen
+#summe der zahlen berechnen und ausgeben
+#differenz der beiden zahlen berechnen und ausgeben
+#produkt berechnen und ausgeben
+#quotient mit nachkommastellen berechnen und ausgeben
+
+print ("Welcome")
+ersteZahl_string = input ("Eingabe erste Zahl:")
+ersteZahl = int (ersteZahl_string)
+zweiteZahl_string = input ("Eingabe zweite Zahl:")
+zweiteZahl = int (zweiteZahl_string)
+Summe = (ersteZahl + zweiteZahl)
+print ("Summe:", Summe)
+Differenz = (ersteZahl - zweiteZahl)
+print ("Differenz:", Differenz)
+Produkt = (ersteZahl * zweiteZahl)
+print ("Produkt:", Produkt)
+Quotient = (ersteZahl / zweiteZahl)
+print ("Quotient:", Quotient)
