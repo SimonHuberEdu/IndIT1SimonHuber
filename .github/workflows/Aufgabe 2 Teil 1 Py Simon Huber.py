ad1b6e39307d60158af296d04581847743ccc646
SimonHuberEdu <80050945+SimonHuberEdu@users.noreply.github.com>
2021-04-05 18:37:31 +0200

    Add files via upload

--- .github/workflows/Aufgabe 2 Teil 1 Py Simon Huber.py
+++ .github/workflows/Aufgabe 2 Teil 1 Py Simon Huber.py
@@ -1,19 +1,19 @@
-# benutzer möglichkeit geben bestimmte anzahl von iterationen zu machen
-# mittels schleife Pi berechnen
-# k ist schleifenvariable
-# weitere variable zum summieren
+# Annäherung Pi/4 mit Leibniz-Reihe von Simon Huber:
+# Leibniz: Summe(von 0 bis unendlich) ((-1)^k)/(2k+1))
 
-# summe(von 0 bis unendlich) ((-1)^k)/(2k+1))
+i = int(input("Eingabe Iterationen:")) # Wie viele Iterationen eingeben
+k = 0                                  # 'k': Laufvariable
+a = 0                                  # Wir fangen von 0 an zu addieren
 
-i = int(input("Eingabe Iterationen:"))
-k = -1
-s = 0
-while k < i:
-    u = (-1)**k / (2*k+1)
-    s = s + u
-    k = k + 1
-    # Möchten Sie die Iterationsschritte angezeigt bekommen, bitte # entfernen
-  # print ("Iterationsschritt:", k)
-  # print ("Annäherung:", s)
+while k < i:                           # Solange Wert(Laufvariable 'k') < Wert(Gewünschte Iterationen 'i') soll Schleife laufen
+    a = a + ((-1)**k / (2*k+1))        # 'a': Funktionswert vorherige Schleife + Funktionswert aktuelle Schleife (bei 1. Iteration: 0 + Funktionswert aktuelle Schleife)
+    k = k + 1                          # 'k': Erhöhe Laufvariable um eins
     
-print ("Pi/4:", s)
+# Möchten Sie die Iterationsschritte angezeigt bekommen, bitte # entfernen:
+    #print ("Iterationsschritt:", k)
+    #print ("Annäherung Pi/4:", a)
+    #print ("_________________________________________") #Für bessere Lesbarkeit/Unterscheidung der einzelnen Iterationen
+
+print ("Ergebnis Pi/4:", a)
+p = 4*a
+print ("Ergebnis Pi:", p)
